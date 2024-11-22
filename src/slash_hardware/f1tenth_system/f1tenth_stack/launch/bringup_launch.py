cd2df1b233883562e502c5eb9a329f56f4839a62
happy_jl <995210764@qq.com>
2024-11-22 16:28:09 +0800

    teb change

--- src/slash_hardware/f1tenth_system/f1tenth_stack/launch/bringup_launch.py
+++ src/slash_hardware/f1tenth_system/f1tenth_stack/launch/bringup_launch.py
@@ -189,14 +189,14 @@ def generate_launch_description():
         parameters=[{
             'target_frame': 'livox_frame',
             'transform_tolerance': 0.01,
-            'min_height': -1.0,
+            'min_height': 0.0,
             'max_height': 0.1,
             'angle_min': -3.14159,  # -M_PI/2
             'angle_max': 3.14159,   # M_PI/2
             'angle_increment': 0.0043,  # M_PI/360.0
             'scan_time': 0.3333,
-            'range_min': 0.45,
-            'range_max': 10.0,
+            'range_min': 0.1,
+            'range_max': 20.0,
             'use_inf': True,
             'inf_epsilon': 1.0
         }],

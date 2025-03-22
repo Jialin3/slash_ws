ca9669f608f73bd0c1087cf38ed344bcee7fca98
szu <995210764@qq.com>
2025-03-22 20:22:42 +0800

    增加依赖

--- src/install_for_humble.sh
+++ src/install_for_humble.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 sudo apt update -y
-sudo apt install -y ros-humble-ros-gzharmonic
 sudo apt install -y ros-humble-joint-state-publisher-gui
 sudo apt install -y ros-humble-sdformat-urdf
 sudo apt install -y ros-humble-robot-state-publisher
+sudo apt install -y ros-humble-ros-gz-sim
+sudo apt install -y ros-humble-ros-gz-bridge
+sudo apt install -y ros-humble-rqt-robot-steering 
 
+#安装gazebo harmonic
+sudo apt install -y curl lsb-release gnupg
+sudo curl https://packages.osrfoundation.org/gazebo.gpg --output /usr/share/keyrings/pkgs-osrf-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/pkgs-osrf-archive-keyring.gpg] http://packages.osrfoundation.org/gazebo/ubuntu-stable $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/gazebo-stable.list > /dev/null
+sudo apt-get update
+# sudo apt-get -y install gz-harmonic
+sudo apt install -y ros-humble-ros-gzharmonic
 
--- src/sim_env.sh
+++ src/sim_env.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # # 设置 Gazebo 资源路径
-export IGN_GAZEBO_RESOURCE_PATH=/home/tang/slash_ws/install/slash_description/share
-export GZ_SIM_RESOURCE_PATH=/home/tang/slash_ws/install/slash_description/share
+export IGN_GAZEBO_RESOURCE_PATH=/home/szu/slash_ws/install/slash_description/share
+export GZ_SIM_RESOURCE_PATH=/home/szu/slash_ws/install/slash_description/share
 
 echo $IGN_GAZEBO_RESOURCE_PATH
 echo $GZ_SIM_RESOURCE_PATH
